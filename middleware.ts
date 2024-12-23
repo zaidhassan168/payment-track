0338125bc38bd729553870e975d561cd714707b6
zaidhassan168 <zaidhassan168@outlook.com>
2024-12-23 17:39:35 +0500

    middle ware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { auth } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/dashboard/:path*"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  const { userId }: { userId: string | null } = await auth()
+  console.log("Request URL:", req.url);
+
+  if (isProtectedRoute(req)) {
+    console.log("Protected route:", req.url);
+    if (!userId) {
+      console.log("No user session, redirecting to landing page");
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+  matcher: ["/dashboard/:path*", "/"],
 };
